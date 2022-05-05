c8af51add2a4eb42bae40f9577e441706b14292e
Charles <charles0527@hotmail.com>
2022-05-05 19:51:10 +0800

    feat: add api error handler & delete restaurant api

--- services/admin-services.js
+++ services/admin-services.js
@@ -1,24 +1,22 @@
 const { Restaurant, Category } = require('../models')
-const { getOffset, getPagination } = require('../helpers/pagination-helper')
 
 const adminServices = {
   getRestaurants: (req, cb) => {
-    const DEFAULT_LIMIT = 9
-    const page = Number(req.query.page) || 1
-    const limit = Number(req.query.limit) || DEFAULT_LIMIT
-    const offset = getOffset(limit, page)
-
-    Restaurant.findAndCountAll({
+    Restaurant.findAll({
       raw: true,
       nest: true,
-      offset,
-      limit,
       include: [Category]
     })
-      .then(restaurants => cb(null, {
-        restaurants: restaurants.rows,
-        pagination: getPagination(limit, page, restaurants.count)
-      }))
+      .then(restaurants => cb(null, { restaurants }))
+      .catch(err => cb(err))
+  },
+  deleteRestaurants: (req, cb) => {
+    Restaurant.findByPk(req.params.id)
+      .then(restaurant => {
+        if (!restaurant) throw new Error("Restaurant didn't exist!")
+        return restaurant.destroy()
+      })
+      .then(deletedRestaurant => cb(null, { restaurant: deletedRestaurant }))
       .catch(err => cb(err))
   }
 }
