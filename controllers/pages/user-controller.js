513a673b3c0a5690170d9d48d6132c70eb88b95a
Charles <charles0527@hotmail.com>
2022-05-08 00:41:45 +0800

    feat: add user signup api

--- controllers/pages/user-controller.js
+++ controllers/pages/user-controller.js
@@ -1,4 +1,4 @@
-const bcrypt = require('bcryptjs')
+// const bcrypt = require('bcryptjs')
 const { User, Restaurant, Comment, Favorite, Like, Followship } = require('../../models')
 const { imgurFileHandler } = require('../../helpers/file-helpers')
 const { getUser } = require('../../helpers/auth-helpers')
@@ -7,32 +7,9 @@ const userController = {
   signUpPage: (req, res) => {
     res.render('signup')
   },
-  signUp: (req, res, next) => {
-    // 修改這裡
-    // 如果兩次輸入的密碼不同，就建立一個 Error 物件並拋出
-    if (req.body.password !== req.body.passwordCheck) {
-      throw new Error('Passwords do not match!')
-    }
-
-    // 確認資料裡面沒有一樣的 email，若有，就建立一個 Error 物件並拋出
-    User.findOne({ where: { email: req.body.email } })
-      .then(user => {
-        if (user) throw new Error('Email already exists!')
-        return bcrypt.hash(req.body.password, 10) // 前面加 return
-      })
-      .then(hash =>
-        User.create({
-          // 上面錯誤狀況都沒發生，就把使用者的資料寫入資料庫
-          name: req.body.name,
-          email: req.body.email,
-          password: hash
-        })
-      )
-      .then(() => {
-        req.flash('success_messages', '成功註冊帳號！') // 並顯示成功訊息
-        res.redirect('/signin')
-      })
-      .catch(err => next(err)) // 接住前面拋出的錯誤，呼叫專門做錯誤處理的 middleware
+  signUp: (req, res) => {
+    req.flash('success_messages', '成功註冊帳號！')
+    res.redirect('/signin')
   },
   signInPage: (req, res) => {
     res.render('signin')
@@ -61,12 +38,13 @@ const userController = {
         if (!checkedUser) throw new Error("User didn't exist!")
         checkedUser = checkedUser.toJSON()
         checkedUser.owner = Number(user.id) === userParamsId || false
-        checkedUser.isFollowed = req.user.Followings.some(
+
+        const isFollowed = user.Followings.some(
           f => f.id === checkedUser.id
         )
         checkedUser.commentedRestaurants = checkedUser.Comments.reduce(
           (accumulator, comment) => {
-            if (!accumulator.some(r => r.id === comment.restaurantId)) {
+            if (!accumulator.some(r => r === comment.restaurantId)) {
               accumulator.push(comment.Restaurant)
             }
             return accumulator
@@ -74,7 +52,8 @@ const userController = {
 
         res.render('users/profile', {
           user,
-          checkedUser
+          checkedUser,
+          isFollowed
         })
       })
       .catch(err => next(err))
